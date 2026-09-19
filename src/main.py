62ba3ce4134a8b7a4c0fba3591f4316b35995b53
Equ17y <antonsobol28@gmail.com>
2026-09-19 23:56:34 +0300

    feat: connect frontend for local testing, serve static files, update .gitignore and docs

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
+
+
+app.mount("/", StaticFiles(directory=str(FRONTEND_DIR), html=True), name="static")
+
+
+@app.get("/")
+async def read_root():
+    index_path = FRONTEND_DIR / "index.html"
+    return FileResponse(str(index_path))
 
 # Тестирование pre-commit хука
